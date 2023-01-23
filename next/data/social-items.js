8b240d488f540eefc50390d9beb03463b2e57df4
Varun Mehta <varunm.mehta@gmail.com>
2023-01-23 20:19:54 +0200

    Social Media Page

---
+++ next/data/social-items.js
@@ -0,0 +1,29 @@
+import Twitter from '../public/twitter.png'
+import Instagram from '../public/instagram.png'
+import LinkedIn from '../public/linkedin.png'
+
+const socialItems = [
+    {
+        title: 'Twitter',
+        subtitle: '@string3',
+        icon: 'icon-twitter',
+        image: Twitter,
+        path: '#',
+    },
+    {
+        title: 'Instagram',
+        subtitle: '@string3_network',
+        icon: 'icon-instagram',
+        image: Instagram,
+        path: '#',
+    },
+    {
+        title: 'LinkedIn',
+        subtitle: '@string3',
+        icon: 'icon-linkedin',
+        image: LinkedIn,
+        path: '#',
+    },
+]
+
+export default socialItems;
