ee8dcaffab91089251b13d91a224a190995b6352
Varun Mehta <varunm.mehta@gmail.com>
2023-01-24 16:37:23 +0200

    Add Head Date

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,17 @@
 import styles from '../styles/Header.module.css'
 
 const Header = () => {
+    const date = new Date()
+    const weekday = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const month = date.toLocaleString('default', { month: 'long' });
+    const weekDay = weekday[date.getDay()]
+    const monthDay = date.getDate()
+
     return (
         <header>
             <div className="container">
                 <div className={styles.Note}>
-                    <h5>Tue, May 18</h5>
+                    <h5>{weekDay}, {month} {monthDay}</h5>
                     <p>145 Active Nodes</p>
                     <p>0.00045kWh</p>
                 </div>
