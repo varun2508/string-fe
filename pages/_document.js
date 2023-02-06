c2ccdbcb10cffda4855f81b706b510b12feb5536
Varun Mehta <varunm.mehta@gmail.com>
2023-02-06 18:44:34 +0200

    Make empty meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,8 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
-                <meta name="description" content="String Marketing Website" />
+                <meta name="description" content="" />
+                <meta name="keywords" content="" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
                 <link rel="icon" href="/favicon.ico" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
