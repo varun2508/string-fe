a53a12f7cb70d9e23dbc566b1798e98e9361d722
Varun Mehta <varunm.mehta@gmail.com>
2023-02-07 12:33:46 +0200

    New favicon

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,9 @@ export default function Document() {
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
                 <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;600&family=Quattrocento+Sans:wght@400;700&display=swap" rel="stylesheet" />
-                <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/favicon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
             </Head>
             <body>
                 <Main />
