818cde8cff1edce39c602d0c40e872dfa48818d9
Varun Mehta <varunm.mehta@gmail.com>
2023-02-13 12:34:11 +0200

    Update API Domain

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ['admin-staging.string3.org']
+    domains: ['107.22.98.205']
   },
   env: {
-    ApiUrl: 'https://admin-staging.string3.org/api',
+    ApiUrl: 'http://107.22.98.205/api',
     ActiveHostedApiUrl:'https://ftnow.api-us1.com/api/3',
     ActiveHostedApiKey:'76ec51edcbae9ea0c4284101ef596cdcee6c6bd804efe4d849d00e70b88b805e5093f310',
     ContactApiUrl: 'https://ftnow.activehosted.com/proc.php',
